59b8d1ff2e72b25a8fd278f5498c434326a9f6ee
DESKTOP-Q1A6VDD\Aparna <a.patllola@gmail.com>
2019-01-01 19:14:35 +0000

    my second project

---
+++ src/MySecondProject/secondProjectclass.java
@@ -0,0 +1,10 @@
+package MySecondProject;
+
+public class secondProjectclass {
+    public static void main(String[] args) {
+        String a;
+        int b= 12;
+        a = "Aparna";
+        System.out.println("Print the name :" + "" + a + b);
+    }
+}
